1dad1ed63d26f7646a443cca4806fd71841bf785
Robin Kuo <robin.kuo.chin.ting@gmail.com>
2025-12-18 23:37:36 +0800

    Fix: Update image paths to be relative for GitHub Pages compatibility

--- src/data/games.js
+++ src/data/games.js
@@ -4,7 +4,7 @@ export const games = [
         title: '2048',
         description: 'Join the numbers and get to the 2048 tile!',
         color: 'bg-yellow-500',
-        image: '/images/cover-2048.png',
+        image: 'images/cover-2048.png',
         link: '/games/2048'
     },
     {
@@ -12,7 +12,7 @@ export const games = [
         title: 'Tetris',
         description: 'The classic block-stacking puzzle game.',
         color: 'bg-purple-600',
-        image: '/images/cover-tetris.png',
+        image: 'images/cover-tetris.png',
         link: '/games/tetris'
     },
     {
@@ -20,7 +20,7 @@ export const games = [
         title: 'Pikachu Volleyball',
         description: 'Legacy web version of the classic game.',
         color: 'bg-yellow-400',
-        image: '/images/cover-pikachu.png',
+        image: 'images/cover-pikachu.png',
         link: '/games/pikachu'
     }
 ];
